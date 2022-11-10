ed9959b4f28bf297bad791526f075f7fa93c83a6
Priya B <priya_saran@yahoo.com>
2022-11-09 16:38:39 -0800

    Added category route to get the categories

--- controllers/api/category-routes.js
+++ controllers/api/category-routes.js
@@ -1,5 +1,19 @@
+const { Category } = require("../../models");
+
 const router = require("express").Router();
 
+router.get('/', async (req, res) => {
+  try {
+    const categoryData = await Category.findAll();
+    if(categoryData) {
+      res.status(200).json(categoryData);
+    } else {
+      res.status(404).json("Not able to read categories");
+    }
+  } catch(err) {
+    res.status(500).json(err);
+  }
+});
 
 
 module.exports = router;
