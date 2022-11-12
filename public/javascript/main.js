05ac4fa54034f3d21697d9806982141cf10748fc
Priya B <priya_saran@yahoo.com>
2022-11-11 17:25:18 -0800

    contains implementing effects using imagefx.js

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -5,3 +5,23 @@ if (logout) {
     document.location.replace("/");
   });
 }
+
+window.onload=function(){
+  var canvas = document.getElementById("source");
+  var context = canvas.getContext("2d");
+  
+  /* Showing a single image with imagefx */
+  //context.drawImage(document.getElementById('img4'),0,0);
+
+  /* iterates through the images every second */
+  /* displaying each as the main large image */
+  
+  let imageArr = ['img1', 'img2', 'img3', 'img4'];
+  let index = 0;
+  var timeInterval = setInterval( () => {
+    index == imageArr.length? index = 0: index++;
+    context.drawImage(document.getElementById(imageArr[index]),0,0);
+  }, 1000);
+  
+
+}
